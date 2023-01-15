04369de758d6d3ed643ab695f2e356d1c2c85d73
Aaron Clark <aaron.clark@emersonwinchester.com>
2023-01-14 18:05:10 -0700

    Fixed code to use script properties
    Instead of using hard coded properties, I chose to use script properties.

--- Code.ts
+++ Code.ts
@@ -1,6 +1,33 @@
 function myFunction() {
+    Logger.log({ logTime: new Date(), message: 'begin inbox unpause' })
+    const myProperty = PropertiesService.getScriptProperties().getProperty;
+    const timeStringToDate = (time: string): Date => {
+        const timeParts = time
+            .split(':')
+            .concat(['0'])
+            .map((part) => Number(part));
+        const now = new Date();
 
-    enum DAYS {
+        return new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            ...timeParts.map((part) => Number(part))
+        );
+    };
+
+    const Prefs = {
+        UNPAUSE_ON_WEEKENDS: JSON.parse(
+            myProperty('UNPAUSE_ON_WEEKENDS') ?? 'false'
+        ) as boolean,
+        UNPAUSE_TIME: timeStringToDate(
+            myProperty('UNPAUSE_TIME') ?? '16:30:00'
+        ),
+    };
+
+    Logger.log(Prefs)
+
+    enum Weekdays {
         SUNDAY = 0,
         MONDAY = 1,
         TUESDAY = 2,
@@ -9,23 +36,22 @@ function myFunction() {
         FRIDAY = 5,
         SATURDAY = 6,
     }
-    const UNPAUSE_AT_HOUR = 16
-    const UNPAUSE_AT_MINUTE = 30
-    const UNPAUSE_ON_WEEKENDS = false
+    const ERROR_AMOUNT = 6 * 60 * 1000; // 6 minutes of range in either direction around the time schedule
 
-    const ERROR_AMOUNT = 9
+    const now = new Date();
+    const currentDay = now.getDay();
+    const currentTime = Number(now);
+    const timeRangeStart = Number(Prefs.UNPAUSE_TIME) - ERROR_AMOUNT;
+    const timeRangeEnd = Number(Prefs.UNPAUSE_TIME) + ERROR_AMOUNT;
 
-    const currentTime = new Date()
-    const currentHour = currentTime.getHours()
-    const currentMinute = currentTime.getMinutes()
-    const currentDay = currentTime.getDay()
+    // Check to see if it's the weekend, and then unpause
+    if (
+        !Prefs.UNPAUSE_ON_WEEKENDS &&
+        [Weekdays.SATURDAY, Weekdays.SUNDAY].includes(currentDay)
+    )
+        return;
 
-    if (!UNPAUSE_ON_WEEKENDS && (currentDay == DAYS.SATURDAY || currentDay == DAYS.SUNDAY)) return;
-    
-    if ( UNPAUSE_AT_HOUR !== currentHour) return;
-    if (currentMinute < UNPAUSE_AT_MINUTE - ERROR_AMOUNT || currentMinute > UNPAUSE_AT_MINUTE + ERROR_AMOUNT) return;
-
-
+    if (currentTime >= timeRangeEnd || currentTime <= timeRangeStart) return;
 
     const paused = GmailApp.getUserLabelByName('Inbox-Paused');
     const threads = paused.getThreads();
